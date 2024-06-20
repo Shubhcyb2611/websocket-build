ee07e6a66bc6691424b4bb3096c0d4e7fff5b449
Shubhcyb2611 <s.gupta@cybtekk.com>
2024-06-20 16:32:48 +0530

    feat: socket server initialized

--- server/index.js
+++ server/index.js
@@ -5,3 +5,16 @@ import { WebSocket } from "ws";
 const app = express();
 
 const port = 8080;
+
+const server = app.listen(port, () => {
+  console.log("server is listening...");
+});
+
+const wss = new WebSocket("ws://localhost:8080/");
+
+// wss.on("open", (ws) => {
+//   ws.on("message", (data) => {
+//     console.log("data from server : ", data);
+//     ws.send("thanks dear !!");
+//   });
+// });
