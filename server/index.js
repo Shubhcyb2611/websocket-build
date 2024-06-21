bd0340a8ed28d9b429493d77b3b95f6446218086
Shubhcyb2611 <s.gupta@cybtekk.com>
2024-06-21 17:28:10 +0530

    fix: config added

--- server/index.js
+++ server/index.js
@@ -1,15 +1,18 @@
 import express from "express";
-
+import http from "http";
 import { WebSocket } from "ws";
 
 const app = express();
 
 const port = 8080;
 
-const server = app.listen(port, () => {
-  console.log("server is listening...");
-});
+const server = http.createServer(app);
 
+app.use(express.static("/client"));
+
+server.listen(posrt, () => {
+  console.log("server is listening");
+});
 const wss = new WebSocket("ws://localhost:8080/");
 
 // wss.on("open", (ws) => {
